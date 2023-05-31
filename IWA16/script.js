63a58c4ad419d7bf96f5ddf56ebfdfe1f8dfc043
bruyns <brandon.bruyns7@gmail.com>
2023-05-31 14:05:04 +0200

    switched JSdoc from IWA12 to IWA16

---
+++ IWA16/script.js
@@ -0,0 +1,128 @@
+const MONTHS = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ]
+  
+  const data = {
+    response: {
+      requestType: "FETCH_ATHLETE_DATA",
+      requestBy: "ALL_MATCHING_ATHLETES",
+      forDisplay: "BEST_RACES",
+  
+      data: {
+        NM372: {
+          firstName: "Nwabisa",
+          surname: "Masiko",
+          id: "NM372",
+          races: [
+            {
+              date: '2022-11-18T20:00:00.000Z',
+              time: [9, 7, 8, 6],
+            },
+            {
+              date: '2022-12-02T20:00:00.000Z',
+              time: [6, 7, 8, 7],
+            },
+          ],
+        },
+  
+        SV782: {
+          firstName: "Schalk",
+          surname: "Venter",
+          id: "SV782",
+          races: [
+            {
+              date: '2022-11-18T20:00:00.000Z',
+              time: [10, 8, 3, 12],
+            },
+            {
+              date: '2022-11-25T20:00:00.000Z',
+              time: [6, 8, 9, 11],
+            },
+            {
+              date: '2022-12-02T20:00:00.000Z',
+              time: [10, 11, 4, 8],
+            },
+            {
+              date: '2022-12-09T20:00:00.000Z',
+              time: [9, 8, 9, 11],
+            },
+          ],
+        },
+      },
+    },
+  };
+  
+  // Only edit below this comment
+  
+  const createHtml = (athlete) => {
+    const {firstName, surname, id, races} = data.response.data[athlete];
+    const {date, time} = races[races.length - 1];
+    // const {date, time} = races.reverse()
+    // [date], [time] = races.reverse()
+    // can reverse if you want to find the last value 1st(would show up as 1st [0]) in the array easier.
+  
+    const fragment = document.createDocumentFragment();
+  
+    const title = document.createElement("h2");
+    title.textContent = `${id}`;
+    fragment.appendChild(title);
+  
+    const list = document.createElement("dl");
+  
+    const eventDate = new Date(date);
+    const day = eventDate.getDate();
+    const month = MONTHS[eventDate.getMonth()];
+    const year = eventDate.getFullYear();
+    // console.log(eventDate)
+  
+   const [first, second, third, fourth] = time;
+   const total = first + second + third + fourth;
+    // const total = 81;
+  
+    const hours = Math.floor(total / 60);     //math.floor what does it do, gives you lowest value
+    const minutes = (total % 60)
+    // console.log(minutes);        //used modulus to give me? remainder of minutes in seconds
+  
+    list.innerHTML = /* html */ `
+      <dt>Athlete</dt>
+      <dd>${firstName} ${surname}</dd>
+  
+      <dt>Total Races</dt>
+      <dd>${races.length}</dd>
+  
+      <dt>Event Date (Latest)</dt>
+      <dd>${day} ${month} ${year}</dd>
+  
+      <dt>Total Time (Latest)</dt>
+      <dd>${hours}:${minutes}</dd>
+    `;
+  
+    fragment.appendChild(list);
+    return fragment
+  }
+  
+  // [NM372], [SV782] = data
+
+  // return fragment
+  const NM372 = document.querySelector('[data-athlete="NM372"]');
+  const SV782 = document.querySelector('[data-athlete="SV782"]');
+  
+  NM372.appendChild(createHtml('NM372'));
+  SV782.appendChild(createHtml('SV782'));
+
+
+  function B ()  {
+    console.log(a);
+    console.log(a)
+  }
